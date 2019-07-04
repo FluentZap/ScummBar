8e71dad959dddbfc3bf6c7d085fa461ac06b7f1e
Todd Aden <FluentZap@Gmail.com>
2019-07-04 11:20:36 -0700

    combine css into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import './App.css';
 import 'typeface-roboto';
 import Navbar from './components/navbar/Navbar';
+import TapCategory from './components/tapcategory/TapCategory';
+import { makeStyles } from '@material-ui/core/styles';
+import woodBackground from './assets/woodBackground.jpg'
+
+
+const useStyles = makeStyles(theme => ({
+  App: {
+    backgroundImage: `url('${woodBackground}')`,
+    width: '100vw',
+    height: '100vh',
+    backgroundPositionX: 'center',
+    backgroundRepeatY: 'no-repeat',
+    backgroundSize: 'cover',    
+  }
+}));
+
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div className={classes.App}>
       <Navbar
-      location="Tap List"
+        location="Tap List"
       />
+      <TapCategory />
     </div>
   );
 }
