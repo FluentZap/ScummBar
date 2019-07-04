a7c3c4b0b68eae3fcb45ba8daf7bfb03f2ad741f
Todd Aden <FluentZap@Gmail.com>
2019-07-04 11:05:50 -0700

    add navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'typeface-roboto';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar
+      location="Tap List"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles, styled } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "center"
+  },
+}));
+
+
+const MyNav = styled(AppBar)({
+  backgroundColor: '#8D6E63'
+});
+
+
+export default function Navbar(props) {
+  const classes = useStyles();
+
+  console.log(props.location);
+  
+  return (
+    <div className={classes.root}>
+      <MyNav position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            {props.location}
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </MyNav>
+    </div>
+  );
+}
